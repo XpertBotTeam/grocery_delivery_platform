5b0ac1a19f1d01113b01831953179d59a8382086
ahmad shakik <ahmadshakik777@gmail.com>
2023-08-04 11:33:44 +0300

    initialize mobile app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'gocery_delivary_project',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Center(
+        child: Text(
+          'Hello, world!',
+        ),
+      ),
+    );
+  }
+}
